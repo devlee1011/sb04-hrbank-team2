d1da45b09e86bdc77ad3257dcac79a96a752b188
NaNHangBok <kms_1015@naver.com>
2025-07-30 15:15:04 +0900

    feat(직원): Employee 및 Controller/Service/Repository 생성
    직원 정보를 담고 있는 Employee 객체 설계 및 생성
    EmployeeController / EmployeeService/ EmployeeRepository/ EmployeeMapper 설계 및 구현
    
    refactor(직원): Employee 및 Controller/Service 수정
    
    추후 merge시 충돌 방지를 위해 스텁메서드 삭제.
    
    feat(직원): Employee 및 Dto, Controller/Service/Repository/Mapper 생성
    
    Employee와 관련된 클래스, Entity, Dto, Controller, Service, Repository, Mapper 생성

--- src/main/java/com/codeit/hrbank/employee/entity/Employee.java
+++ src/main/java/com/codeit/hrbank/employee/entity/Employee.java
@@ -1,11 +1,14 @@
 package com.codeit.hrbank.employee.entity;
 
 import com.codeit.hrbank.base_entity.BaseUpdatableEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.codeit.hrbank.department.entity.Department;
+import com.codeit.hrbank.stored_file.entity.StoredFile;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -13,4 +16,30 @@ import lombok.Setter;
 @Entity
 @Table(name = "employees")
 public class Employee extends BaseUpdatableEntity {
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String position;
+
+    @Column(nullable = false)
+    LocalDate hireDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    EmployeeStatus status;
+
+    @Column
+    String employee_number;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    Department department;
+
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE}, orphanRemoval = true)
+    @JoinColumn(name = "profile_id")
+    StoredFile profile;
 }
