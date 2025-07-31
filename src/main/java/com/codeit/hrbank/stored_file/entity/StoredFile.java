27240484c7c970510a12973bce982ac1c9817cd7
Oh-Myeongjae <omjcws@gmail.com>
2025-07-31 13:22:54 +0900

    feat(파일관리, 백업관리): 파일관리, 백업관리를 위한 기본 엔티티, 컨트롤러, 서비스 구현

--- src/main/java/com/codeit/hrbank/stored_file/entity/StoredFile.java
+++ src/main/java/com/codeit/hrbank/stored_file/entity/StoredFile.java
@@ -1,7 +1,9 @@
 package com.codeit.hrbank.stored_file.entity;
 
 import com.codeit.hrbank.base_entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,4 +14,12 @@ import lombok.Setter;
 @Entity
 @Table(name = "files")
 public class StoredFile extends BaseEntity {
+  @Column(nullable = false)
+  private String fileName;
+
+  @Column(nullable = false)
+  private String type;
+
+  @Column(nullable = false)
+  private Long size;
 }
