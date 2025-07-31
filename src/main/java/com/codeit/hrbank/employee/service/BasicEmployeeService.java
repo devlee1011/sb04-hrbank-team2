9274ed94e89fc91459ca2f162952c01544271cd8
NaNHangBok <kms_1015@naver.com>
2025-07-31 14:13:35 +0900

    feat(직원): 직원 상제 조회 기능 구현
    직원 상제 조회 기능 구현
    직원 Id로 EmployeeDto 반환

--- src/main/java/com/codeit/hrbank/employee/service/BasicEmployeeService.java
+++ src/main/java/com/codeit/hrbank/employee/service/BasicEmployeeService.java
@@ -1,7 +1,10 @@
 package com.codeit.hrbank.employee.service;
 
 import com.codeit.hrbank.department.repository.DepartmentRepository;
+import com.codeit.hrbank.employee.entity.Employee;
 import com.codeit.hrbank.employee.repository.EmployeeRepository;
+import com.codeit.hrbank.exception.BusinessLogicException;
+import com.codeit.hrbank.exception.ExceptionCode;
 import com.codeit.hrbank.stored_file.repository.StoredFileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
@@ -14,4 +17,10 @@ public class BasicEmployeeService implements EmployeeService {
     private final DepartmentRepository departmentRepository;
     private final ApplicationEventPublisher eventPublisher;
     private final StoredFileRepository storedFileRepository;
+
+    @Override
+    public Employee getById(Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new BusinessLogicException(ExceptionCode.EMPLOYEE_NOT_FOUND));
+        return employee;
+    }
 }
--- src/main/java/com/codeit/hrbank/employee/service/EmployeeService.java
+++ src/main/java/com/codeit/hrbank/employee/service/EmployeeService.java
@@ -1,4 +1,7 @@
 package com.codeit.hrbank.employee.service;
 
+import com.codeit.hrbank.employee.entity.Employee;
+
 public interface EmployeeService {
+    Employee getById(Long id);
 }
