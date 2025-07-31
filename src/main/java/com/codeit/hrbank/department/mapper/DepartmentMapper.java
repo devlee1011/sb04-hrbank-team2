b834e8e2ef6dd50da98177efca1bf6e6f9992a52
JihyeonLee <devlee1011@gmail.com>
2025-07-31 15:00:49 +0900

    refactor(부서등록): 매퍼에서 establishedDate가 null일 때 현재 날짜로 자동으로 설정되게 변경

--- src/main/java/com/codeit/hrbank/department/mapper/DepartmentMapper.java
+++ src/main/java/com/codeit/hrbank/department/mapper/DepartmentMapper.java
@@ -4,10 +4,14 @@ import com.codeit.hrbank.department.dto.request.DepartmentCreateRequest;
 import com.codeit.hrbank.department.dto.response.DepartmentDto;
 import com.codeit.hrbank.department.entity.Department;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {
 
-    Department toEntity (DepartmentCreateRequest departmentCreateRequest);
-    DepartmentDto toDto (Department department, Long employeeCount);
+    @Mapping(target = "establishedDate",
+            expression = "java(departmentCreateRequest.establishedDate() != null ? departmentCreateRequest.establishedDate() : java.time.LocalDate.now())")
+    Department toEntity(DepartmentCreateRequest departmentCreateRequest);
+
+    DepartmentDto toDto(Department department, Long employeeCount);
 }
