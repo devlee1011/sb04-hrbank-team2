6330cbdccb9c1c55c0615681bd01a86475ba571f
NaNHangBok <kms_1015@naver.com>
2025-08-02 00:51:59 +0900

    feat(수정이력): 수정이력 상세 조회 임시저장
    수정이력 상세 조회 임시저장

--- src/main/java/com/codeit/hrbank/change_log/controller/ChangeLogController.java
+++ src/main/java/com/codeit/hrbank/change_log/controller/ChangeLogController.java
@@ -2,10 +2,17 @@ package com.codeit.hrbank.change_log.controller;
 
 import com.codeit.hrbank.change_log.service.ChangeLogService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ChangeLogController {
     private final ChangeLogService changeLogService;
+
+    @GetMapping("/{id}")
+    public DiffDto get(@PathVariable("id") Long changeLogId){
+
+    }
 }
