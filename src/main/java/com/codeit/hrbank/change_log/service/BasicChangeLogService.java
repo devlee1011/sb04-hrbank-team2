9925988387e38c3926f21e585b6655f36981fc6f
NaNHangBok <kms_1015@naver.com>
2025-08-02 01:12:33 +0900

    feat(수정이력): 수정이력 상세 조회 기능 추가
    수정이력 상세 조회 기능 추가

--- src/main/java/com/codeit/hrbank/change_log/service/BasicChangeLogService.java
+++ src/main/java/com/codeit/hrbank/change_log/service/BasicChangeLogService.java
@@ -1,8 +1,11 @@
 package com.codeit.hrbank.change_log.service;
 
+import com.codeit.hrbank.change_log.entity.ChangeLogDetail;
 import com.codeit.hrbank.change_log.repository.ChangeLogDetailRepository;
 import com.codeit.hrbank.change_log.repository.ChangeLogRepository;
 import com.codeit.hrbank.employee.repository.EmployeeRepository;
+import com.codeit.hrbank.exception.BusinessLogicException;
+import com.codeit.hrbank.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,4 +15,9 @@ public class BasicChangeLogService implements ChangeLogService {
     private final ChangeLogRepository changeLogRepository;
     private final ChangeLogDetailRepository changeLogDetailRepository;
     private final EmployeeRepository employeeRepository;
+
+    @Override
+    public ChangeLogDetail getChangeLogDetail(Long id) {
+        return changeLogDetailRepository.findById(id).orElseThrow(() -> new BusinessLogicException(ExceptionCode.LOG_NOT_FOUND));
+    }
 }
--- src/main/java/com/codeit/hrbank/change_log/service/ChangeLogService.java
+++ src/main/java/com/codeit/hrbank/change_log/service/ChangeLogService.java
@@ -1,4 +1,7 @@
 package com.codeit.hrbank.change_log.service;
 
+import com.codeit.hrbank.change_log.entity.ChangeLogDetail;
+
 public interface ChangeLogService {
+    ChangeLogDetail getChangeLogDetail(Long id);
 }
