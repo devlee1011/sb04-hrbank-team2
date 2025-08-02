1b6b49227bd854b4ea90816c01371f1bdf760d34
JihyeonLee <devlee1011@gmail.com>
2025-08-02 22:09:57 +0900

    feat(부서): 부서 삭제 기능 구현

--- src/main/java/com/codeit/hrbank/department/service/DepartmentService.java
+++ src/main/java/com/codeit/hrbank/department/service/DepartmentService.java
@@ -1,11 +1,7 @@
 package com.codeit.hrbank.department.service;
 
-import com.codeit.hrbank.department.dto.request.DepartmentGetAllRequest;
-import com.codeit.hrbank.department.entity.Department;
-import org.springframework.data.domain.Page;
-
 public interface DepartmentService {
-    Page<Department> getAllDepartments(DepartmentGetAllRequest pageRequest);
+    void delete(Long id);
 
     Long getEmployeeCountByDepartmentId(Long departmentId);
 }
