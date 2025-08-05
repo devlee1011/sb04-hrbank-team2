5a5bb69f87f89881eb4016d8a23f90f61965066d
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2025-08-05 11:27:31 +0900

    Delete src/main/resources/static directory

--- src/main/resources/static/src/components/dashboard/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as EmployeeDistributionChart } from './EmployeeDistributionChart';
-export { default as EmployeeStatusCard } from './EmployeeStatusCard';
-export { default as EmployeeTrendChart } from './EmployeeTrendChart';
--- src/main/resources/static/src/styles/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import 'styled-components';
-import { Theme } from './theme';
-
-// styled-components의 DefaultTheme 인터페이스 확장
-declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
-} 
